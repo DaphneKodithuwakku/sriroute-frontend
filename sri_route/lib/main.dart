48f049a51a7bbc35cfa5bb8e567235fa16850874
SD/ <shay6dc@gmail.com>
2025-03-18 02:20:53 +0530

    Updated title

--- sri_route/lib/main.dart
+++ sri_route/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
+      title: 'SriRoute',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
