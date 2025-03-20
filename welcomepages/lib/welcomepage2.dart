f87dd3e23240d497f1386257f5c56555b8a6373d
SD/ <shay6dc@gmail.com>
2025-03-20 23:05:02 +0530

    Fixed errors in welcomepage2.dart

--- welcomepages/lib/welcomepage2.dart
+++ welcomepages/lib/welcomepage2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage1 extends StatelessWidget {
-  const WelcomePage1({super.key});
+class WelcomePage2 extends StatelessWidget {
+  const WelcomePage2({super.key});
 
   @override
   Widget build(BuildContext context) {
