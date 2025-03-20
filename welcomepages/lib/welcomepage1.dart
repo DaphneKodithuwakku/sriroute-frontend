0c0a5a2c5959a8287dbf3bb80c72d7fc9cde01c7
SD/ <shay6dc@gmail.com>
2025-03-20 23:21:22 +0530

    Fixed the image alignment in welcomepage1.dart

--- welcomepages/lib/welcomepage1.dart
+++ welcomepages/lib/welcomepage1.dart
@@ -11,7 +11,7 @@ class WelcomePage1 extends StatelessWidget {
         children: [
           // Background Image
           Positioned.fill(
-            child: Image.asset('assets/welcomepage1.png', fit: BoxFit.cover),
+            child: Image.asset('assets/welcomepage1.png', fit: BoxFit.fitWidth, alignment: Alignment.topCenter),
           ),
           // Bottom Card with Content
           Align(
