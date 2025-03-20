7add35b75f82b8b308d44ce86b934edceff91091
SD/ <shay6dc@gmail.com>
2025-03-20 23:15:39 +0530

    Revert "Fixed the image background in welcomepage2.png as well"
    This reverts commit 1e183f7eb392e21cf0a5858e31638a836e8bb026.

--- welcomepages/lib/welcomepage1.dart
+++ welcomepages/lib/welcomepage1.dart
@@ -11,7 +11,7 @@ class WelcomePage1 extends StatelessWidget {
         children: [
           // Background Image
           Positioned.fill(
-            child: Image.asset('assets/welcomepage1.png', fit: BoxFit.fitWidth),
+            child: Image.asset('assets/welcomepage1.png', fit: BoxFit.cover),
           ),
           // Bottom Card with Content
           Align(
