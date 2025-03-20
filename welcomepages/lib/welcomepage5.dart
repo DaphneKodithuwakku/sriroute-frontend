b5a5f6eb7937e34842bada5d6d660275708b51a4
SD/ <shay6dc@gmail.com>
2025-03-20 23:56:21 +0530

    Changed the navigator step's color

--- welcomepages/lib/welcomepage5.dart
+++ welcomepages/lib/welcomepage5.dart
@@ -66,7 +66,7 @@ class WelcomePage5 extends StatelessWidget {
                         child: Icon(
                           Icons.circle,
                           size: 10,
-                          color: index == 3 ? Colors.black : Colors.grey,
+                          color: index == 4 ? Colors.black : Colors.grey,
                         ),
                       );
                     }),
