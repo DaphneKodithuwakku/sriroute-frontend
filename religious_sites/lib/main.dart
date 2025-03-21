90d45316399482810d691b64731520e17523e1ec
DaphneKodithuwakku <kodithuwakkudaphne@gmail.com>
2025-03-21 09:36:48 +0530

    Created the ReligiousSitesPage Screen

--- religious_sites/lib/main.dart
+++ religious_sites/lib/main.dart
@@ -5,8 +5,84 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: ReligiousSitesPage());
+  }
+}
+
+class ReligiousSitesPage extends StatefulWidget {
+  const ReligiousSitesPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ReligiousSitesPageState createState() => _ReligiousSitesPageState();
+}
+
+class _ReligiousSitesPageState extends State<ReligiousSitesPage> {
+  String? selectedReligion;
+  String? selectedDistrict;
+
+  final List<String> religions = [
+    "Buddhism",
+    "Christianity",
+    "Hinduism",
+    "Islam",
+  ];
+  final List<String> districts = [
+    "Colombo",
+    "Kandy",
+    "Jaffna",
+    "Galle",
+    "Anuradhapura",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Religious Sites in Sri Lanka")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(labelText: "Select Religion"),
+              value: selectedReligion,
+              items:
+                  religions.map((religion) {
+                    return DropdownMenuItem<String>(
+                      value: religion,
+                      child: Text(religion),
+                    );
+                  }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedReligion = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(labelText: "Select District"),
+              value: selectedDistrict,
+              items:
+                  districts.map((district) {
+                    return DropdownMenuItem<String>(
+                      value: district,
+                      child: Text(district),
+                    );
+                  }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedDistrict = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
