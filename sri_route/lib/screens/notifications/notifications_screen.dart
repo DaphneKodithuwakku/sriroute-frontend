f2c3cc08aa6e95129175806b2471ebe2246f33ff
Amharyoosuf <ahramr12089@gmail.com>
2025-03-23 20:41:37 +0530

    updated all notifications as read when opening the screen

--- sri_route/lib/screens/notifications/notifications_screen.dart
+++ sri_route/lib/screens/notifications/notifications_screen.dart
@@ -9,3 +9,10 @@ class NotificationsScreen extends StatefulWidget {
   @override
   _NotificationsScreenState createState() => _NotificationsScreenState();
 }
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Mark all notifications as read when opening the screen
+    NotificationService.markAllAsRead();
+  }
