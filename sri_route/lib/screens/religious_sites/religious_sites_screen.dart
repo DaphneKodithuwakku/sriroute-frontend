73f84a48cacd717acf6bb40c1e287f1fb4334a5f
DaphneKodithuwakku <kodithuwakkudaphne@gmail.com>
2025-03-24 01:04:07 +0530

    Implement bottom navigation bar with custom icons and styling

--- sri_route/lib/screens/religious_sites/religious_sites_screen.dart
+++ sri_route/lib/screens/religious_sites/religious_sites_screen.dart
@@ -42,3 +42,75 @@ class _MyHomePageState extends State<MyHomePage> {
       _selectedIndex = index;
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _widgetOptions),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+        child: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home, color: Colors.white, size: 24),
+                onPressed: () => _onItemTapped(0),
+                color:
+                    _selectedIndex == 0
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.7),
+              ),
+              IconButton(
+                icon: SimpleVRGlassesIcon(
+                  color:
+                      _selectedIndex == 1
+                          ? Colors.white
+                          : Colors.white.withOpacity(0.7),
+                  size: 34,
+                ),
+                onPressed: () => _onItemTapped(1),
+              ),
+              IconButton(
+                icon: Icon(Icons.search, color: Colors.white, size: 24),
+                onPressed: () => _onItemTapped(2),
+                color:
+                    _selectedIndex == 2
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.7),
+              ),
+              IconButton(
+                icon: Icon(Icons.menu_book, color: Colors.white, size: 24),
+                onPressed: () => _onItemTapped(3),
+                color:
+                    _selectedIndex == 3
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.7),
+              ),
+              IconButton(
+                icon: Icon(Icons.person, color: Colors.white, size: 24),
+                onPressed: () => _onItemTapped(4),
+                color:
+                    _selectedIndex == 4
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.7),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
