1b79deb9b3c0a470ae5fbc60e18ce40d2c87b7a0
Amharyoosuf <ahramr12089@gmail.com>
2025-03-23 20:40:49 +0530

    started Notification events

---
+++ sri_route/lib/screens/notifications/notifications_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../models/notification_model.dart';
+import '../../services/notification_service.dart';
+
+class NotificationsScreen extends StatefulWidget {
+  const NotificationsScreen({Key? key}) : super(key: key);
+
+  @override
+  _NotificationsScreenState createState() => _NotificationsScreenState();
+}
