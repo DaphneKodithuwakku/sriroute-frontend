1fe73517ff218e98af8fbc2df42d4cf67892e6fe
SayuDiwakara <168662423+SayuDiwakara@users.noreply.github.com>
2025-03-24 00:04:21 +0530

    Create MyHomePage with bottom navigation

--- sri_route/lib/widgets/bottom_nav_bar.dart
+++ sri_route/lib/widgets/bottom_nav_bar.dart
@@ -14,3 +14,25 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages or screens (replace with your actual screens)
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Screen'),
+    Text('VR Tour Screen'),
+    Text('Pilgrimage Planner Screen'),
+    Text('User Manual Screen'),
+    Text('Profile Screen'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
